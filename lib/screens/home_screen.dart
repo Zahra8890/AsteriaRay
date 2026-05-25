03317a5b024202124860a43ea97cf5835d3a2c09
Zahra8890 <161909079+Zahra8890@users.noreply.github.com>
2026-05-25 16:37:34 -0700

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -50,7 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Future<void> _loadServers() async {
     setState(() => _isLoadingServers = true);
     try {
-      final response = await http.get(Uri.parse('https://srv.rsfly.pro/srv.txt'));
+      final response = await http.get(Uri.parse('http://srv.rsfly.pro/srv.txt'));
       if (response.statusCode == 200) {
         setState(() {
           _servers = response.body
